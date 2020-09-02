3bf47d882e158a9978dd014b7acdaec22b9bc9ad
lssuseendharlal <lssuseendharlal@gmail.com>
2020-09-02 16:16:50 +0530

    projects component added

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,39 @@ import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
 
-export default () => {
+export default ({
+  data: {
+    allStrapiProjects: {
+      nodes: { projects },
+    },
+  },
+}) => {
   return (
     <Layout>
       <Hero />
       <Services />
+      {/* <Jobs /> */}
+      <Projects projects={projects} title="featured projects" showLink />
     </Layout>
   )
 }
-// ...GatsbyImageSharpFluid
+export const featuredProjects = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
