e3930ee314a633fb70020dfbba0bec20e14404f6
lssuseendharlal <lssuseendharlal@gmail.com>
2020-09-05 17:44:20 +0530

    style changed

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,7 +9,7 @@ const Hero = () => {
     query {
       file(relativePath: { eq: "susee-bw.png" }) {
         childImageSharp {
-          fixed(height: 650, width: 500) {
+          fixed(height: 650, width: 650) {
             ...GatsbyImageSharpFixed
           }
         }
@@ -27,7 +27,7 @@ const Hero = () => {
         <article className="hero-info">
           <div>
             <div className="underline"></div>
-            <h1>I'm Suseendhar Lal</h1>
+            <h1>I'm Suseendhar</h1>
             <h4>UI developer</h4>
             <Link to="/contact" className="btn">
               contact me
