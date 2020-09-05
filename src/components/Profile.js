6bb89538a3e34f2b200ba068da3d9a3abbe7e101
unknown <lssuseendharlal@gmail.com>
2020-09-05 15:37:54 +0530

    profile img changed

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,7 +7,7 @@ import SocialLinks from "../constants/socialLinks"
 const Hero = () => {
   const data = useStaticQuery(graphql`
     query {
-      file(relativePath: { eq: "susee.jpg" }) {
+      file(relativePath: { eq: "susee-orig.jpg" }) {
         childImageSharp {
           fixed(height: 650, width: 500) {
             ...GatsbyImageSharpFixed
