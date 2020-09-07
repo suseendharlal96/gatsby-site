59e6e0e92351b98fcfe1cfbc89d5230e48684253
lssuseendharlal <lssuseendharlal@gmail.com>
2020-09-07 22:05:48 +0530

    tags pagination added

--- src/templates/SingleTag.js
+++ src/templates/SingleTag.js
@@ -2,6 +2,7 @@ import React from "react"
 import Projects from "../components/Projects"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ProjectPagination from "../components/ProjectPagination"
 
 const SingleTag = ({ data, pageContext: { tag } }) => {
   return (
