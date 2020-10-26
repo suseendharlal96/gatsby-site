b8f1d8ddc7a387782cf9e1e6f2d3e7e8973c1c41
unknown <lssuseendharlal@gmail.com>
2020-10-26 13:36:03 +0530

    navbar border condition added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,20 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 import { FaAlignRight } from "react-icons/fa"
 import PageLinks from "./links"
 const Navbar = ({ toggleSidebar }) => {
-  // const [header, setHeader] = useState(false)
-  // useEffect(() => {
-  //   window.addEventListener("scroll", () => {
-  //     window.scrollY > 100 ? setHeader(true) : setHeader(false)
-  //   })
-  //   return () => {
-  //     const removeFunction = () => {}
-  //     window.removeEventListener("scroll", removeFunction)
-  //   }
-  // }, [])
+  const [header, setHeader] = useState(false)
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false)
+    })
+    return () => {
+      const removeFunction = () => {}
+      window.removeEventListener("scroll", removeFunction)
+    }
+  }, [])
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${header ? "border" : ""}`}>
       <div className="nav-center">
         <div className="nav-header">
           <Link to="/">
