cb0c47d37b2fd860271424b34f9a9060370cd28d
unknown <lssuseendharlal@gmail.com>
2020-11-17 19:09:36 +0530

    scroll top added

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,37 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 // import "../css/main.css" check gatsby-browser file in root directory
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
 const Layout = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [header, setHeader] = useState(false)
   const toggleSidebar = () => {
     setIsOpen(!isOpen)
   }
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 100 ? setHeader(true) : setHeader(false)
+    })
+    return () => {
+      const removeFunction = () => {}
+      window.removeEventListener("scroll", removeFunction)
+    }
+  }, [])
   return (
     <>
       <Navbar toggleSidebar={toggleSidebar} />
       <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
       <div className="content-animate">{children}</div>
+      {header && (
+        <button
+          className="content-animate scroll-btn"
+          onClick={() => window.scroll({ top: 0, behavior: "smooth" })}
+        >
+          <span className="line-1"></span>
+          <span className="line-2"></span>
+        </button>
+      )}
       <Footer />
     </>
   )
